142ac49aa61aabbace1e95c0d1d749a24e58d9f9
kmh <kmh1011@hs.ac.kr>
2020-07-06 06:47:09 +0900

    1

--- movies/views.py
+++ movies/views.py
@@ -14,18 +14,14 @@ def detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'movies/detail.html', {'movie': movie})
 
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#     except(KeyError, Choice.DoesNotExist):
-#         return render(request, 'polls/detail.html', {'question': question, 'error_message': "you didn't select a choice."})
-#
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
-#
-# def results(request, question_id):
-#         question = get_object_or_404(Question, pk=question_id)
-#         return render(request, 'polls/results.html', {'question': question})
+def movie_list(request):
+    qs = movie.objects.all()
+
+    q = request.GET.get('q', '') # GET request의 인자중에 q 값이 있으면 가져오고, 없으면 빈 문자열 넣기
+    if q: # q가 있으면
+        qs = qs.filter(title__icontains=q) # 제목에 q가 포함되어 있는 레코드만 필터링
+
+    return render(request, 'movies/movie_list.html', {
+        'movie_list' : qs,
+        'q' : q,
+    })
